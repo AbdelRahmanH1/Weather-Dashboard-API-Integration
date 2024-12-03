8e5cc60597921b85ad1580b863ebf0ad5acddfff
Abdel Rahman <hossamabdelrahman62@gmail.com>
2024-12-03 19:43:57 +0200

    Update reponse message

--- src/utils/asyncHandler.util.js
+++ src/utils/asyncHandler.util.js
@@ -1,10 +1,9 @@
 const asyncHandler = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch((e) => {
-      const err = new Error(
-        e.message || 'Something went wrong in async handler',
-        { cause: e.cause || 500 },
-      );
+      const err = new Error(e.message || 'Internal Server Error', {
+        cause: e.cause || 500,
+      });
       return next(err);
     });
   };
--- src/utils/globalErrorHandler.util.js
+++ src/utils/globalErrorHandler.util.js
@@ -1,9 +1,7 @@
 const globaErrorHandler = (err, req, res, next) => {
   const statusCode = err.cause || 500;
-  const message = err.message || 'Something went wrong';
+  const message = err.message || 'Internal Server Error';
 
-  return res
-    .status(statusCode)
-    .json({ success: false, message, stack: err.stack });
+  return res.status(statusCode).json({ success: false, message });
 };
 export default globaErrorHandler;
